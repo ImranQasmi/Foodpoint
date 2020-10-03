63a6c020954429ef4dca6c43f6845b92efe519a6
ImranQasmi <mi1871925@gmail.com>
2020-10-03 09:28:05 -0700

    Animations Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -13,26 +13,22 @@ import { Console } from 'console';
 
 import { Comment } from '../shared/comment';
 import { from } from 'rxjs';
-
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { flyInOut,expand } from '../animations/app.animation';
+import { visibility } from '../animations/app.animation';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
-  animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
-  ]
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+    animations: [
+      visibility(),
+      flyInOut(),
+      expand()
+    ]
 })
 
 export class DishdetailComponent implements OnInit {
